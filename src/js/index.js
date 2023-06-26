a362916202a557e938b9f2ab08598687fc92f147
VladislavGaydukov <vladislav1997gaydukov@gmail.com>
2023-06-26 18:30:07 +0400

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,16 @@ const swiper = new Swiper('.swiper', {
     clickable: true,
   },  
 });
+
+const dropDownMenu = document.querySelector('.menu');
+const burgerButton = document.querySelector('.header__button--menu')
+const outButton = document.querySelector('.menu__button--out')
+
+burgerButton.addEventListener('click', function () {
+  dropDownMenu.classList.add('menu-visibility');
+  outButton.classList.remove('menu__button--visibility');
+});
+
+outButton.addEventListener('click', function () {
+  dropDownMenu.classList.remove('menu-visibility');
+});
